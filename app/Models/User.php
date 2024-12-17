f9ac1ac57c06e5fabab2b5dcd12992eb24e3fa61
noydono <th.gabillard@laposte.net>
2024-12-17 11:18:57 +0100

    add birthdate field to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,8 @@ class User extends Authenticatable
         'email',
         'password',
         'job',
-        'picture'
+        'picture',
+        'birthdate',
     ];
 
     public function likes(){
