b4af653bfe18fa0f75bfdba052847fa705cd966f
noydono <th.gabillard@laposte.net>
2024-12-18 11:17:18 +0100

    Created seeder for hobbies table KAN-54

--- database/migrations/04_create_hobbies_table.php
+++ database/migrations/04_create_hobbies_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('hobbie', function (Blueprint $table) {
+        Schema::create('hobbies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('type');
