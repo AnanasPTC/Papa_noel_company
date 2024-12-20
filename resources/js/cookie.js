2584f18eb07318ee60f744339cc7f8c199a851c8
noydono <th.gabillard@laposte.net>
2024-12-20 09:09:59 +0100

    add easter egg

--- resources/js/cookie.js
+++ resources/js/cookie.js
@@ -1,1 +1,169 @@
+let cookies = 0
+
+let rate = 2
+
+let cursorPrice = 10
+let rollerPrice = 50
+let mamiePrice = 100
+let usinePrice = 1000
+
+let globalInterval
+
+
+let cursorWinPerSeconde = 0.1
+let rollerWinPerSeconde = 0.5
+let mamieWinPerSeconde = 5
+let usineWinPerSeconde = 50
+
+let cursorNumber = 0
+let rollerNumber = 0
+let mamieNumber = 0
+let usineNumber = 0
+
+let cookieDom = document.querySelector('#cookieImgContainer')
+let cookieDomImg = document.querySelector('#cookieImgContainer').firstChild.nextSibling
+let cookieNumberDom = document.querySelector('#cookieNumbersContainer')
+
+let cursorPercent = document.querySelector('#cursorPercent')
+let rollerPercent = document.querySelector('#rollerPercent')
+let mamiePercent = document.querySelector('#mamiePercent')
+let usinePercent = document.querySelector('#usinePercent')
+
+let cursorBtn = document.querySelector('#addCursor')
+let rollerBtn = document.querySelector('#addRoller')
+let mamieBtn = document.querySelector('#addMamie')
+let usineBtn = document.querySelector('#addUsine')
+
+let cursor = document.querySelector('#cursor')
+let roller = document.querySelector('#roller')
+let mamie = document.querySelector('#mamie')
+let usine = document.querySelector('#usine')
+
+let usineImg = "<img class=\"usine\" src=\"/secret/usine.png\" height=\"100\" width=\"100\">"
+let mamieImg = "<img class=\"mamie\" src=\"/secret/mamie.png\" height=\"100\" width=\"100\">"
+let rollerImg ="<img class=\"roller\" src=\"/secret/rouleaux.png\" height=\"100\" width=\"100\">"
+let cursorImg ="<img class=\"cursor\" src=\"/secret/cursor.png\" height=\"100\" width=\"100\">"
+
+cookieDom.addEventListener('click', () => {
+    cookies++
+    updateCookies()
+
+    if (cookies >= cursorPrice && cursorBtn.hasAttribute('disabled')){
+        cursorBtn.removeAttribute('disabled')
+    }
+    if (cookies < cursorPrice && !cursorBtn.hasAttribute('disabled')){
+        cursorBtn.setAttribute('disabled','disabled')
+    }
+    if (cookies >= rollerPrice && rollerBtn.hasAttribute('disabled')){
+        rollerBtn.removeAttribute('disabled')
+    }
+    if (cookies < rollerPrice && !rollerBtn.hasAttribute('disabled')){
+        rollerBtn.setAttribute('disabled','disabled')
+    }
+    if (cookies >= mamiePrice && mamieBtn.hasAttribute('disabled')){
+        mamieBtn.removeAttribute('disabled')
+    }
+    if (cookies < mamiePrice && !mamieBtn.hasAttribute('disabled')){
+        mamieBtn.setAttribute('disabled','disabled')
+    }
+    if (cookies >= usinePrice && usineBtn.hasAttribute('disabled')){
+        usineBtn.removeAttribute('disabled')
+    }
+    if (cookies < usinePrice && !usineBtn.hasAttribute('disabled')){
+        usineBtn.setAttribute('disabled','disabled')
+    }
+
+    changeScale(cookieDomImg, 1.5)
+    setTimeout( () => {
+        changeScale(cookieDomImg, 1)
+    },100)
+
+    if (cookies === 1000000){
+        clearInterval(golbalInterval)
+    }
+})
+
+cursorBtn.addEventListener('click', () => {
+    if (cookies >= cursorPrice && !cursorBtn.hasAttribute('disabled')){
+        cursorNumber++
+        cookies = cookies - cursorPrice
+        setGlobalInterval(cursorWinPerSeconde, cursorNumber)
+        cursor.innerHTML += cursorImg
+        cursorPrice  += rate * cursorPrice  / 100
+        cursorPercent.textContent = (cursorWinPerSeconde * cursorNumber).toFixed(2)
+        if (cookies <= cursorPrice){
+            cursorBtn.setAttribute('disabled','disabled')
+        }
+    }
+})
+
+rollerBtn.addEventListener('click', () => {
+    if (cookies >= rollerPrice && !rollerBtn.hasAttribute('disabled')){
+        rollerNumber++
+        cookies = cookies - rollerPrice
+        setGlobalInterval(rollerWinPerSeconde, rollerNumber)
+        roller.innerHTML += rollerImg
+        rollerPrice  += rate * rollerPrice  / 100
+        rollerPercent.textContent = (rollerWinPerSeconde * rollerNumber).toFixed(2)
+        if (cookies <= rollerPrice){
+            rollerBtn.setAttribute('disabled','disabled')
+        }
+    }
+})
+//
+// mamieBtn.addEventListener('click', () => {
+//     if (cookies >= mamiePrice && !mamieBtn.hasAttribute('disabled')){
+//         mamieNumber++
+//         clearInterval(mamieInterval)
+//         mamieInterval = setInterval(() =>{
+//             cookies+=(mamieWinPerSeconde * mamieNumber)
+//             updateCookies()
+//         },1000)
+//         mamie.innerHTML += mamieImg
+//         cookies = cookies - mamiePrice
+//         mamiePrice  += rate * mamiePrice  / 100
+//         mamiePercent.textContent = (mamieWinPerSeconde * mamieNumber).toFixed(2)
+//         if (cookies <= mamiePrice){
+//             mamieBtn.setAttribute('disabled','disabled')
+//         }
+//     }
+// })
+//
+// usineBtn.addEventListener('click', () => {
+//     if (cookies >= usinePrice && !usineBtn.hasAttribute('disabled')){
+//         usineNumber++
+//         clearInterval(usineInterval)
+//         usineInterval = setInterval(() =>{
+//             cookies+=(usineWinPerSeconde * usineNumber)
+//             updateCookies()
+//         },1000)
+//         usine.innerHTML += usineImg
+//         cookies = cookies - usinePrice
+//         usinePrice  += rate * usinePrice  / 100
+//         usinePercent.textContent = (usineWinPerSeconde * usineNumber).toFixed(2)
+//         if (cookies <= usinePrice){
+//             usineBtn.setAttribute('disabled','disabled')
+//         }
+//     }
+// })
+
+function setGlobalInterval(perSeconde, numberOfItems) {
+    let winPerSecond = 0
+    winPerSecond += (perSeconde * numberOfItems)
+    clearInterval(globalInterval)
+    globalInterval = setInterval(() =>{
+        cookies+= winPerSecond
+        console.log(cookies)
+        console.log(winPerSecond)
+        updateCookies()
+    },1000)
+}
+
+function changeScale (domElement , percent) {
+    return domElement.style.transform = `scale(${percent})`
+}
+
+function updateCookies(){
+    cookieNumberDom.textContent = Math.round(cookies)
+}
 
--- resources/views/pages/secret/cookie.blade.php
+++ resources/views/pages/secret/cookie.blade.php
@@ -5,21 +5,65 @@
         <h1 class="cookie_title">Lutin Cliqueur</h1>
         <div class="cookie_container">
             <div class="cookie_container_leftside">
-                <div class="cookie_container_leftside_image">
-                    <div class="cookie_container_leftside_image">
-                        <div class="cookie_container_leftside_image_img"></div>
-                    </div>
-                    <div class="cookie_container_leftside_splach">
-                        <img class="cookie_container_leftside_splach-big" src="/secret/bigsplach.svg" height="100" width="100" alt="">
-                        <img class="cookie_container_leftside_splach-small" src="/secret/lisplach.svg" height="100" width="100" alt="">
-                    </div>
+                <div id="cookieImgContainer" class="cookie_container_leftside_image">
+                    <div class="cookie_container_leftside_image_img"></div>
+                    <h2>Clique</h2>
                 </div>
             </div>
-            <div class="cookie_container_right">
-                <img class="lilsplach" src="/secret/usine.png" height="100" width="100" alt="">
-                <img class="lilsplach" src="/secret/mamie.png" height="100" width="100" alt="">
-                <img class="lilsplach" src="/secret/rouleaux.png" height="100" width="100" alt="">
-                <img class="lilsplach" src="/secret/cursor.png" height="100" width="100" alt="">
+            <div class="cookie_container_rightside">
+                <div class="cookie_container_rightside_score">
+                    <span>Vous avez </span>
+                    <p id="cookieNumbersContainer">0</p><span> Lutin Biscuit</span>
+                </div>
+                <div class="cookie_container_rightside_spell">
+                    <button id="addCursor" class="btn btn-primary" disabled="disabled">Ajouter un cursor</button>
+                    <button id="addRoller" class="btn btn-primary" disabled>Ajouter un rouleaux</button>
+                    <button id="addMamie" class="btn btn-primary" disabled>Ajouter une mamie</button>
+                    <button id="addUsine" class="btn btn-primary" disabled>Ajouter une usine</button>
+                </div>
+                <div class="cookie_container_rightside_board">
+                    <div class="cookie_container_rightside_board_cursor">
+                        <div class="cookie_container_rightside_board_cursor_header">
+                            <h2>Vos Cursor</h2>
+                            <p id="cursorPercent" class="cookie_container_rightside_board_cursor_header_percent"></p> <span>/s</span>
+                        </div>
+                        <div id="cursor">
+
+                        </div>
+                    </div>
+
+                    <div class="cookie_container_rightside_board_roller">
+                        <div class="cookie_container_rightside_board_roller_header">
+                            <h2>Vos Rouleaux a patisserie</h2>
+                            <p id="rollerPercent" class="cookie_container_rightside_board_roller_header_percent"></p><span>/s</span>
+                        </div>
+                        <div id="roller">
+
+                        </div>
+                    </div>
+
+                    <div class="cookie_container_rightside_board_mamie">
+                        <div class="cookie_container_rightside_board_mamie_header">
+                            <h2>Vos mamie</h2>
+                            <p id="mamiePercent" class="cookie_container_rightside_board_mamie_header_percent"></p>
+                            <span>/s</span>
+                        </div>
+                        <div id="mamie">
+
+                        </div>
+                    </div>
+
+                    <div class="cookie_container_rightside_board_usine">
+                        <div class="cookie_container_rightside_board_usine_header">
+                            <h2>Vos Rouleaux a usine</h2>
+                            <p id="usinePercent" class="cookie_container_rightside_board_usine_header_percent"></p>
+                            <span>/s</span>
+                        </div>
+                        <div id="usine">
+
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
