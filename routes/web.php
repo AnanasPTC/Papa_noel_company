06650dc7d0aa9f4268e41f3e73eee0dd08993f1c
Nadege <nadege04gautier@gmail.com>
2024-12-20 09:39:15 +0100

    + modif suite

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,24 @@ use App\Http\Controllers\ProfileController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::resource('profile',ProfileController::class)->except(['create', 'store']);
+Route::resource('profile', ProfileController::class)->except(['create', 'store']);
 
+// Recherche
 Route::get('/search', [SearchController::class, 'index'])->name('search.index');
 Route::post('/search', [SearchController::class, 'findByFilter'])->name('search.filter');
 
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+// Profiles
 Route::get('/profile/create', [ProfileController::class, 'create'])->name('profile.create');
 Route::post('/profile', [ProfileController::class,'store'])->name('profile.store');
 Route::get('/profile/{id}', [ProfileController::class, 'show'])->name('profile.show');
 Route::get('/profile/{id}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::put('/profile/{id}', [ProfileController::class, 'update'])->name('profile.update');
 Route::delete('/profile/{id}', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+// Utilisateurs
 Route::delete('/users/{id}', [ProfileController::class, 'destroy'])->name('users.destroy');
-Route::put('/user/profile', [ProfileController::class, 'updateProfile']);
-Route::patch('/users/{id}', [ProfileController::class, 'updateStatus'])->name('users.updateStatus');
-Route::patch('/users/{id}', [ProfileController::class, 'update'])->name('users');
-Route::get('/users/{id}/edit', [ProfileController::class, 'editProfile'])->name('users.editProfile');
+Route::patch('/users/{id}/status', [ProfileController::class, 'updateStatus'])->name('users.updateStatus');
+Route::post('/users/{id}/edit', [ProfileController::class, 'editProfile'])->name('users.editProfile');
 Route::get('/user/profile', [ProfileController::class, 'profile'])->name('user.profile');
 Route::put('/user/profile', [ProfileController::class, 'update'])->name('user.update');
 
