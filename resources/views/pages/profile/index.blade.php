a3a1b8637fb9dc6caa6c1e7422c6eac6b4821c8e
Nadege <nadege04gautier@gmail.com>
2024-12-20 12:06:18 +0100

    modif dans index et show

--- resources/views/pages/profile/index.blade.php
+++ resources/views/pages/profile/index.blade.php
@@ -4,25 +4,33 @@
 <div class="container py-5">
     <div class="row">
         <div class="col-md-12">
-            <h1>Profil de {{ $user->name }}</h1>
-            <p>Voici les informations de votre profil :</p>
-            <ul>
-                <li>Nom : {{ $user->name }}</li>
-                <li>Prénom : {{ $user->first_name }}</li>
-                <li>Email : {{ $user->email }}</li>
-                <li>Date de naissance : {{ \Carbon\Carbon::parse($user->birthdate)->format('d-m-Y') }}</li>
-                <li>Hobbies :
-                    <ul>
-                        @foreach($user->hobbies as $hobby)
-                            <li>{{ $hobby }}</li>
-                        @endforeach
-                    </ul>
-                </li>
-                <li>Photo : <img src="{{ $user->photo }}" alt="Photo de profil" class="img-fluid"></li>
-            </ul>
-            @if (Auth::id() == $user->id)
-                <a href="{{ route('profile.edit', $user->id) }}" class="btn btn-primary">Modifier mon profil</a>
-            @endif
+            <div class="form-group">
+                <h1>Bienvenue les lutins!</h1>
+                <p>Vous êtes sur le site officiel des rencontres entre lutins de la Papa Noel Compagny.</p>
+                <h2>Page profile de {{ $user->name }}</h2>
+                <p>Voici les informations de votre profil:</p>
+                <ul>
+                    <li>
+                        <div class="avatar avatar-circle xxl mt-n5 mb-3">
+                            <img class="avatar-img rounded-circle border border-white border-3" src="{{ $user->picture }}" alt="Ma Photo de profil" class="img-fluid" src="{{ $user->picture }}"></li>
+                    <li>Nom: {{ $user->lastname }}</li>
+                    <li>Prénom: {{ $user->firstname }}</li>
+                    <li>
+                        <div>
+                            <div class="form-floating">
+
+                            </div>
+                        </div>
+                    </li>
+                    <li>Job: {{ $user->job }}</li>
+                    <li>Email: {{ $user->email }}</li>
+                    <li>Date de naissance : {{ \Carbon\Carbon::parse($user->birthdate)->format('d-m-Y') }}</li>
+                </ul>
+                @if (Auth::id() == $user->id)
+                    <a href="{{ route('profile.edit', $user->id) }}" class="btn btn-primary">Modifier mon profil</a>
+                    <a href="{{ route('logout') }}" class="btn btn-danger">Se déconnecter</a>
+                @endif
+            </div>
         </div>
     </div>
 </div>
