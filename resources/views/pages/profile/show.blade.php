c9e08d6dfb6835440700f8b0908514e858c0a0d0
Nadege <nadege04gautier@gmail.com>
2024-12-20 10:58:39 +0100

    mofif users dans table hobbies.users suppression tokens de sacrum mofif show blade

--- resources/views/pages/profile/show.blade.php
+++ resources/views/pages/profile/show.blade.php
@@ -10,11 +10,11 @@
                 <h2>Page profile de {{ $user->name }}</h2>
                 <p>Voici les informations de votre profil:</p>
                 <ul>
-                    <li>Nom: {{ $user->name }}</li>
-                    <li>Prénom: {{ $user->first_name }}</li>
+                    <li>Nom: {{ $user->lastname }}</li>
+                    <li>Prénom: {{ $user->firstname }}</li>
                     <li>Email: {{ $user->email }}</li>
                     <li>Date de naissance: {{ $user->birthdate->format('d-m-Y') }}</li>
-                    <li>Photo: <img src="{{ $user->photo }}" alt="Photo de profil" class="img-fluid"></li>
+                    <li>Photo: <img src="{{ $user->picture }}" alt="Photo de profil" class="img-fluid"></li>
                 </ul>
                 @if (Auth::id() == $user->id)
                     <a href="{{ route('profile.edit', $user->id) }}" class="btn btn-primary">Modifier mon profil</a>
