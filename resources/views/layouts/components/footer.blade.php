62b2fb9533209d9c3adcd5641566d7a17313699f
Nadege <nadege04gautier@gmail.com>
2024-12-20 16:41:49 +0100

    footer

---
+++ resources/views/layouts/components/footer.blade.php
@@ -0,0 +1,13 @@
+<div class="container my-5">
+
+    <footer class="text-center text-lg-start" style="background-color: #fc0808;">
+      <!-- Copyright -->
+      <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-white" href="https://papa-No.lcompagny.com/">papa-No.lcompagny.com</a>
+      </div>
+      <!-- Copyright -->
+    </footer>
+
+  </div>
+  <!-- End of .container -->
